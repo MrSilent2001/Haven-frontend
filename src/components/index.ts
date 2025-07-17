a0ca70ed03ff30757114decc7b65b80338982e10
MrSilent2001 <110489250+MrSilent2001@users.noreply.github.com>
2025-07-17 08:30:43 +0530

    Revert "Refactor App structure and update dependencies"

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-// Centralized component exports
-export { default as Button } from './Button/Button';
-// Add more components here as they are created
-// export { default as Input } from './Input';
-// export { default as Card } from './Card';
-// export { default as Modal } from './Modal'; 
--- src/screens/index.ts
+++
@@ -1,7 +0,0 @@
-// Centralized screen exports
-export { default as HomeScreen } from './HomeScreen';
-// Add more screens here as they are created
-// export { default as ProfileScreen } from './ProfileScreen';
-// export { default as SettingsScreen } from './SettingsScreen';
-export { default as LoginScreen } from './LoginScreen'; 
-export { default as RegisterScreen } from './RegisterScreen'; 
